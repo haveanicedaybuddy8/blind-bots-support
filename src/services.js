82fca4d29c93d639d9249962ae18a622d86c42cd
Bobby Wen <haveanicedaybuddy8@gmail.com>
2026-01-06 01:56:17 -0500

    change port

--- src/services.js
+++ src/services.js
@@ -10,8 +10,8 @@ const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY
 
 const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
-    port: 465,
-    secure: true,
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS
